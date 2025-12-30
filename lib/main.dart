a990553279e72ce404e7cb6b15589b0c03b230ce
zihadulislam2 <zihadul708@gmail.com>
2025-12-31 05:25:36 +0600

    Add Windows runner files for Flutter application
    - Created a manifest file for the Windows runner to support DPI awareness and compatibility with Windows 10 and 11.
    - Implemented utility functions in utils.cpp and utils.h for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion.
    - Developed the Win32Window class in win32_window.cpp and win32_window.h to manage window creation, message handling, and theme updates based on system preferences.

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+String name = 'Zihadul Islam';
+int number = 1;
+double num = 1.7;
+List myList = ['String', 'sdkojlk'];
+Map<String, dynamic> myMap = {'String': 'String', 'String2': 4444};
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // wrap with container
+        // Right click on any weiget => refactor => wrap with container
+        child: Container(
+          height: double.infinity,
+          width: 100.0,
+          padding: EdgeInsets.all(50.0),
+          decoration: BoxDecoration(color: Colors.red),
+          child: (Text("hello", style: TextStyle(color: Colors.green))),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
